600ecc7c3e30820ee83069e4a21caff8a3c1f017
DariaDesiderata <daria.calwell@gmail.com>
2020-07-01 12:57:31 -0600

    🛠update base url

--- fe_react/src/services/applicationServices.js
+++ fe_react/src/services/applicationServices.js
@@ -1,10 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const getQuestions = async () => {
+  const baseURL = process.env.REACT_APP_BASE_URL;
   try {
-    const response = await axios.get('http://localhost:1337/question')
-    return response.data
+    const response = await axios.get(`${baseURL}/question`);
+    return response.data;
   } catch (err) {
-    throw err
+    throw err;
   }
-}
+};
